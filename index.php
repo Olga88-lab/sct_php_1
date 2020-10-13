4bc294f56dd8d81aa33b0152647987a309958c38
Olga88_lab <olga_mikhaleva88@mai.ru>
2020-10-14 00:27:00 +0300

    Урок 3

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sample page</title>
+</head>
+<body>
+ <h1> <?php echo "Здравствуй, дорогой читатель!"?> </h1>
+ <p>Text</p>
+ <a class="list" href="index.php">Перейти на главную страницу</a></br>
+ <a class="list" href="php/1.php">Перейти на 1-ю страницу</a></br>
+ <a class="list" href="php/2.php">Перейти на 2-ю страницу</a>
+</body>
+</html>
