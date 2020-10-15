ce8ced5fe2008b906a41c0207ada57cc6bfca1ad
Olga88_lab <olga_mikhaleva88@mai.ru>
2020-10-15 20:23:27 +0300

    Урок 4

---
+++ 1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sample page</title>
+</head>
+<body>
+ <h1> <?php echo "Здравствуй, дорогой читатель!" ?> </h1>
+ <p>Text</p>
+ <h2> <?php echo"Файл 1.php"?> </h2>
+ <p>Text</p>
+ <a href="<?='index.php'?>">Перейти на главную страницу</a></br>
+ <a href="<?='1.php'?>">Перейти на 1-ю страницу</a></br>
+ <a href="<?='2.php'?>">Перейти на 2-ю страницу</a>
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,10 +4,20 @@
     <title>Sample page</title>
 </head>
 <body>
- <h1> <?php echo "Здравствуй, дорогой читатель!"?> </h1>
- <p>Text</p>
- <a class="list" href="index.php">Перейти на главную страницу</a></br>
- <a class="list" href="php/1.php">Перейти на 1-ю страницу</a></br>
- <a class="list" href="php/2.php">Перейти на 2-ю страницу</a>
+    <?php
+    echo "<h1>Здравствуй, дорогой читатель, мы рады тебе!</h1>"?>
+    <p> <img src="<? echo 'images/1.jpg';?>"
+    width="280" height="189" alt=""></a></p>
+    <?php
+    echo "<p>Пусть каждое твоё начинание приводит к успеху!</p>"?>
+    <p> <img src="<? echo 'images/2.jpg';?>"
+    width="280" height="189" alt=""></a></p>
+    <?php
+    echo "<p>Верь в себя!</p>"?>
+    <p> <img src="<? echo 'images/3.jpg';?>"
+    width="280" height="189" alt=""></a></p>
+<a href="<?='index.php'?>">Перейти на главную страницу</a></br>
+<a href="<?='1.php'?>">Перейти на 1-ю страницу</a></br>
+<a href="<?='2.php'?>">Перейти на 2-ю страницу</a>
 </body>
 </html>
