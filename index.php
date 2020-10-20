308339aa835581fe421d7cd2954fa2f7f600a6c8
Olga88_lab <olga_mikhaleva88@mai.ru>
2020-10-20 22:02:01 +0300

    Урок 4 правки

--- index.php
+++ index.php
@@ -7,17 +7,17 @@
     <?php
     echo "<h1>Здравствуй, дорогой читатель, мы рады тебе!</h1>"?>
     <p> <img src="<? echo 'images/1.jpg';?>"
-    width="280" height="189" alt=""></a></p>
+    width="280" height="189" alt=""></p>
     <?php
     echo "<p>Пусть каждое твоё начинание приводит к успеху!</p>"?>
     <p> <img src="<? echo 'images/2.jpg';?>"
-    width="280" height="189" alt=""></a></p>
+    width="280" height="189" alt=""></p>
     <?php
     echo "<p>Верь в себя!</p>"?>
     <p> <img src="<? echo 'images/3.jpg';?>"
-    width="280" height="189" alt=""></a></p>
-<a href="<?='index.php'?>">Перейти на главную страницу</a></br>
-<a href="<?='1.php'?>">Перейти на 1-ю страницу</a></br>
+    width="280" height="189" alt=""></p>
+<a href="<?='index.php'?>">Перейти на главную страницу</a><br>
+<a href="<?='1.php'?>">Перейти на 1-ю страницу</a><br>
 <a href="<?='2.php'?>">Перейти на 2-ю страницу</a>
 </body>
 </html>
