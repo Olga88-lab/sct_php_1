60b48e5051dbf1cb608f9ab549683ac97293b4f4
Olga88_lab <olga_mikhaleva88@mai.ru>
2020-10-27 17:05:35 +0300

    Урок 5 правки

--- pages/2.php
+++ pages/2.php
@@ -14,16 +14,16 @@ $pages2 = '2.php';
     <title>Картинки</title>
 </head>
 <body>
-<p><img src="<?= $img1 ?>"
-         width="280" height="189" alt=""></p>
-<p><img src="<?= $img2 ?>"
-         width="280" height="189" alt=""></p>
-<p><img src="<?= $img3 ?>"
-         width="280" height="189" alt=""></p>
-<p><img src="<?= $img4 ?>"
-         width="280" height="189" alt=""></p>
-<p><img src="<?= $img5 ?>"
-         width="280" height="189" alt=""></p>
+<p>
+    <img src="<?= $img1 ?>" width="280" height="189" alt=""></p>
+<p>
+    <img src="<?= $img2 ?>" width="280" height="189" alt=""></p>
+<p>
+    <img src="<?= $img3 ?>" width="280" height="189" alt=""></p>
+<p>
+    <img src="<?= $img4 ?>" width="280" height="189" alt=""></p>
+<p>
+    <img src="<?= $img5 ?>" width="280" height="189" alt=""></p>
 <ul>
     <li><a href="<?= $index ?>">Перейти на главную страницу</a><br></li>
     <li><a href="<?= $pages1 ?>">Перейти на 1-ю страницу</a><br></li>
