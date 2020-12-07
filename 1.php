7e329f60febd8efd72e28ed2d3445170fbf54e95
Olga88_lab <olga_mikhaleva88@mai.ru>
2020-12-07 21:34:06 +0300

    Урок 12

--- 1.php
+++ 1.php
@@ -1,2 +1,41 @@
+<html>
+<body>
 <?php
-Новый
+/**
+ * Функция вывода данных о человеке
+ * @param $name если данный параметр не введен, имя человека не выводится
+ * @param string $age если данный параметр не введен, возраст человека не выводится
+ * @param string $address если данный параметр не введен, адрес человека не выводится
+ * @return string
+ */
+function letText ($name, $age = "", $address = "")
+{
+    if ($age !== '') {
+        if ($age%10 == 0) {
+        $text = 'лет' ;
+        }
+        if ($age%10 == 1) {
+        $text = 'год' ;
+        }
+        elseif ($age%10 > 1 && $age%10 < 5) {
+        $text = 'года' ;
+        }
+        else  if ($age%10 > 4 && $age%10 < 21) {
+        $text = 'лет' ;
+        }
+        echo  "$name $age $text $address";
+    }
+    elseif ($age == '') {
+    return "$name $address";
+    }
+    elseif ($name == '') {
+    return "$age $address";
+    }
+    elseif ($address == '') {
+    return "$name $age";
+    }
+}
+echo letText ('Кира', '34', 'Санкт-Петербург, ул.Колыменская, д. 93, кв. 12');
+?>
+</ body >
+</ html >
---
+++ 2.php
@@ -0,0 +1,53 @@
+<html>
+<body>
+<?php
+/**
+ * @param $a если данный параметр не является числом, то значение приводится к int
+ * @param $b если данный параметр не является числом, то значение приводится к int
+ * @param string $c параметр функции не обязателен
+ * @return int
+ */
+function result($a, $b, $c = "")
+{
+    if ($a !=='' && $a !== null and $a <= $b and $a <= $c ) {
+        return (int) $a;
+    }
+    if ($a == '' or $a == null) {
+        if ($b <= $c) {
+            return (int) $b;
+        }
+        if ($c < $b) {
+            return (int) $c;
+        }
+    }
+    elseif ($b !== '' && $b !== null and $b < $a and $b <= $c ) {
+        return (int) $b;
+    }
+    if ($b == '' or $b == null) {
+        {
+            if ($a <= $c) {
+                return (int) $a;
+            }
+            if ($c < $a) {
+                return (int) $c;
+            }
+        }
+    }
+    elseif ($c !== '' && $c !== null and $c < $a and $c < $b) {
+        return (int) $c;
+    }
+    if ($c == '' or $c == null) {
+        {
+            if ($b <= $a) {
+                return (int) $b;
+            }
+            if ($a < $b) {
+                return (int) $a;
+            }
+        }
+    }
+}
+echo result(10,26, -1);
+?>
+</body>
+</html></html>
