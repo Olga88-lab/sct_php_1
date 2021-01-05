359e09673f6e626813545739c5a23566a9277969
Olga88_lab <olga_mikhaleva88@mai.ru>
2021-01-05 22:19:15 +0300

    Урок 14 корректировка

--- 2.php
+++ 2.php
@@ -3,20 +3,23 @@
 <?php
     function mb_ucfirst($string, $enc = 'UTF-8')
     {
-        return mb_strtoupper(mb_substr($string, 0, 1, $enc), $enc).
+    return mb_strtoupper(mb_substr($string, 0, 1, $enc), $enc).
         mb_substr($string, 1, mb_strlen($string, $enc), $enc);
     }
-    $str = "ПРИВетСТВУю, дАМЫ И ГОСПоДа!";
-    $str = mb_ucfirst(mb_strtolower($str));
-    $a = array (" " => "_");
-    if ($str[strlen($str) - 1] == '.' or $str[strlen($str)-1] == '!'
-        or $str[strlen($str) - 1] == ',' or $str[strlen($str) - 1] == '?')
+    function name ()
     {
-        echo strtr ($str, $a);
-    } else {
-        $str = trim($str) . ".";
-        echo strtr ($str, $a);
+        $str = "ПРИВетСТВУю, дАМЫ И ГОСПоДа!";
+        $str = mb_ucfirst(mb_strtolower($str));
+        $a = array(" " => "_");
+        if ($str[strlen($str) - 1] == '.' or $str[strlen($str) - 1] == '!'
+            or $str[strlen($str) - 1] == ',' or $str[strlen($str) - 1] == '?') {
+            echo strtr($str, $a);
+        } else {
+            $str = trim($str) . ".";
+            echo strtr($str, $a);
+        }
     }
+    name ();
 ?>
 </body>
 </html>
